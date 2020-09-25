f4f543c604eab2cdf5a3eccaf3ce91ba14e130b8
meyer <meyer_net@foxmail.com>
2020-09-26 01:25:28 +0800

    Update preset-terminal-tools.sh
    test

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -19,8 +19,9 @@ git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-
+echo 21
 # Get .zshrc dotfile
 cp ../../../scripts/.zshrc .
-
+echo 22
 popd
+echo 23
